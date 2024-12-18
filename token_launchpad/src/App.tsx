47a500c92af4b38d36b7d2eddce68db564d88309
rohits-web03 <rohit.dey302003@gmail.com>
2024-12-19 05:23:41 +0530

    Add Wallet adapters and Polyfills

--- token_launchpad/src/App.tsx
+++ token_launchpad/src/App.tsx
@@ -1,11 +1,31 @@
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
+import TokenLaunchPad from './components/TokenLaunchPad';
+
 function App() {
   return (
-    <>
-      <div className="h-screen bg-black text-white">
-        <h1>Token Lauchpad</h1>
-      </div>
-    </>
+    <div className='h-screen bg-black text-white'>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+            <WalletModalProvider>
+              <div className='flex justify-between px-16 py-6'>
+                <p className='text-2xl font-bold'>Token Launchpad</p>
+                <div className='flex justify-center gap-8'>
+                  <WalletMultiButton />
+                  <WalletDisconnectButton />
+                </div>
+              </div>
+              <TokenLaunchPad></TokenLaunchPad>
+            </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ token_launchpad/src/components/TokenLaunchPad.tsx
@@ -0,0 +1,7 @@
+const TokenLaunchPad = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default TokenLaunchPad
